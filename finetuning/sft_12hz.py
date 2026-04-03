02cb46f138306e97432343c352a0b027ffeec481
Tran Hoang The Linh <117964350+thelinh246@users.noreply.github.com>
2026-04-03 14:36:24 +0700

    Refactor model calls to use unwrapped talker model

--- finetuning/sft_12hz.py
+++ finetuning/sft_12hz.py
@@ -136,8 +136,11 @@ def train():
                 input_text_ids = input_ids[:, :, 0]
                 input_codec_ids = input_ids[:, :, 1]
 
-                input_text_embedding = model.talker.base_model.model.text_embedding(input_text_ids) * text_embedding_mask
-                input_codec_embedding = model.talker.base_model.model.codec_embedding(input_codec_ids) * codec_embedding_mask
+                # Lấy talker base model bất kể bị wrap bao nhiêu tầng (LoRA + accelerator)
+                talker_inner = accelerator.unwrap_model(model).talker.base_model.model
+
+                input_text_embedding = talker_inner.text_embedding(input_text_ids) * text_embedding_mask
+                input_codec_embedding = talker_inner.codec_embedding(input_codec_ids) * codec_embedding_mask
 
                 talker_hidden_size = input_codec_embedding.shape[-1]
                 if input_text_embedding.shape[-1] != talker_hidden_size:
@@ -147,7 +150,7 @@ def train():
                 input_embeddings = input_text_embedding + input_codec_embedding
 
                 for i in range(1, 16):
-                    codec_i_embedding = model.talker.base_model.model.code_predictor.get_input_embeddings()[i - 1](codec_ids[:, :, i])
+                    codec_i_embedding = talker_inner.code_predictor.get_input_embeddings()[i - 1](codec_ids[:, :, i])
                     codec_i_embedding = codec_i_embedding * codec_mask.unsqueeze(-1)
                     if input_embeddings.shape[-1] != codec_i_embedding.shape[-1]:
                         pad_size = input_embeddings.shape[-1] - codec_i_embedding.shape[-1]
@@ -165,11 +168,11 @@ def train():
                 talker_hidden_states = hidden_states[codec_mask[:, :-1]]
                 talker_codec_ids = codec_ids[codec_mask]
 
-                sub_talker_hidden_size = model.talker.base_model.model.code_predictor.config.hidden_size
+                sub_talker_hidden_size = talker_inner.code_predictor.config.hidden_size
                 if talker_hidden_states.shape[-1] != sub_talker_hidden_size:
                     talker_hidden_states = talker_hidden_states[..., :sub_talker_hidden_size]
 
-                sub_talker_logits, sub_talker_loss = model.talker.base_model.model.forward_sub_talker_finetune(
+                sub_talker_logits, sub_talker_loss = talker_inner.forward_sub_talker_finetune(
                     talker_codec_ids, talker_hidden_states
                 )
 
